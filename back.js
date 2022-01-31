0c319077347bc6e0c37a10c9ef26beec5e9c3375
Felipe Ventura <fmagven93@gmail.com>
2022-01-31 18:19:38 -0300

    slight validation bug fix

--- back.js
+++ back.js
@@ -22,7 +22,7 @@ const participantSchema = joi.object({
 const messageSchema = joi.object({
     to: joi.string().required(),
     text: joi.string().required(),
-    type: joi.string().required()
+    type: joi.string().valid('message', 'private_message')
 })
 
 app.post('/participants', async(req, res) =>{
